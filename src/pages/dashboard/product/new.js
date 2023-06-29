5b79dc9717778e8644285fcaa74e2ae6e2596fdd
Le Kiet <kiethohohoho@gmail.com>
2023-06-29 21:48:18 +0700

    halinh first commit

---
+++ src/pages/dashboard/product/new.js
@@ -0,0 +1,58 @@
+import { Box, IconButton, Tooltip } from '@mui/material';
+import { useSnackbar } from 'notistack';
+import { useState } from 'react';
+import { Helmet } from 'react-helmet-async';
+import Iconify from 'src/components/iconify';
+import { useSettingsContext } from 'src/components/settings';
+import { primaryPresets } from 'src/theme/options/presets';
+import axiosInstance, { endpoints } from 'src/utils/axios';
+// sections
+// import { ProductCreateView } from 'src/sections/product/view';
+
+// ----------------------------------------------------------------------
+
+export default function ProductCreatePage() {
+  const { themeColorPresets } = useSettingsContext();
+  const color = primaryPresets.filter((c) => c.name === themeColorPresets)[0].main;
+  const { enqueueSnackbar } = useSnackbar();
+  const [voucher, setVoucher] = useState()
+
+  const handleGenerateVoucher = () => {
+    axiosInstance.post(endpoints.product.gen)
+      .then(({ data }) => {
+        enqueueSnackbar('Generate success!');
+        setVoucher(data);
+      }).catch((err) => {
+        enqueueSnackbar('Generate failed!', {
+          variant: 'error'
+        });
+      })
+  };
+
+  return (
+    <>
+      <Helmet>
+        <title> Dashboard: Generate a new voucher</title>
+      </Helmet>
+
+      <Box>
+        <Tooltip title="Generate voucher">
+          <IconButton
+            color={color}
+            onClick={handleGenerateVoucher}
+          >
+            <Iconify width={30} icon="mdi:auto-mode" />
+          </IconButton>
+        </Tooltip>
+        {
+          voucher &&
+          <Box sx={{ mt: 3 }}>
+            {voucher.value}
+          </Box>
+        }
+
+        {/* <ProductCreateView /> */}
+      </Box >
+    </>
+  )
+}
---
+++ src/sections/product/product-table-row.js
@@ -0,0 +1,138 @@
+import { format } from 'date-fns';
+import PropTypes from 'prop-types';
+// @mui
+import Button from '@mui/material/Button';
+import Checkbox from '@mui/material/Checkbox';
+import IconButton from '@mui/material/IconButton';
+import ListItemText from '@mui/material/ListItemText';
+import MenuItem from '@mui/material/MenuItem';
+import TableCell from '@mui/material/TableCell';
+import TableRow from '@mui/material/TableRow';
+// utils
+// hooks
+import { useBoolean } from 'src/hooks/use-boolean';
+// components
+import { ConfirmDialog } from 'src/components/custom-dialog';
+import CustomPopover, { usePopover } from 'src/components/custom-popover';
+import Iconify from 'src/components/iconify';
+import Label from 'src/components/label';
+
+// ----------------------------------------------------------------------
+
+export default function ProductTableRow({
+  row,
+  selected,
+  onSelectRow,
+  onDeleteRow,
+  onEditRow,
+  onViewRow,
+}) {
+  const {
+    value,
+    createDate,
+    expired,
+    status,
+  } = row;
+
+  const confirm = useBoolean();
+
+  const popover = usePopover();
+
+  return (
+    <>
+      <TableRow hover selected={selected}>
+        <TableCell padding="checkbox">
+          <Checkbox checked={selected} onClick={onSelectRow} />
+        </TableCell>
+
+        <TableCell>{value}</TableCell>
+
+        <TableCell>
+          <ListItemText
+            primary={format(new Date(createDate), 'dd MMM yyyy')}
+            secondary={format(new Date(createDate), 'p')}
+            primaryTypographyProps={{ typography: 'body2', noWrap: true }}
+            secondaryTypographyProps={{
+              mt: 0.5,
+              component: 'span',
+              typography: 'caption',
+            }}
+          />
+        </TableCell>
+
+        <TableCell>{expired}</TableCell>
+
+        <TableCell>
+          <Label variant="soft" color={(status === 'có sẵn' && 'info') || 'default'}>
+            {status}
+          </Label>
+        </TableCell>
+
+        <TableCell align="right">
+          <IconButton color={popover.open ? 'primary' : 'default'} onClick={popover.onOpen}>
+            <Iconify icon="eva:more-vertical-fill" />
+          </IconButton>
+        </TableCell>
+      </TableRow>
+
+      <CustomPopover
+        open={popover.open}
+        onClose={popover.onClose}
+        arrow="right-top"
+        sx={{ width: 140 }}
+      >
+        <MenuItem
+          onClick={() => {
+            onViewRow();
+            popover.onClose();
+          }}
+        >
+          <Iconify icon="solar:eye-bold" />
+          View
+        </MenuItem>
+
+        <MenuItem
+          onClick={() => {
+            onEditRow();
+            popover.onClose();
+          }}
+        >
+          <Iconify icon="solar:pen-bold" />
+          Edit
+        </MenuItem>
+
+        <MenuItem
+          onClick={() => {
+            confirm.onTrue();
+            popover.onClose();
+          }}
+          sx={{ color: 'error.main' }}
+        >
+          <Iconify icon="solar:trash-bin-trash-bold" />
+          Delete
+        </MenuItem>
+      </CustomPopover>
+
+      <ConfirmDialog
+        open={confirm.value}
+        onClose={confirm.onFalse}
+        title="Delete"
+        content="Are you sure want to delete?"
+        action={
+          <Button variant="contained" color="error" onClick={onDeleteRow}>
+            Delete
+          </Button>
+        }
+      />
+    </>
+  );
+}
+
+ProductTableRow.propTypes = {
+  onDeleteRow: PropTypes.func,
+  onEditRow: PropTypes.func,
+  onSelectRow: PropTypes.func,
+  onViewRow: PropTypes.func,
+  row: PropTypes.object,
+  selected: PropTypes.bool,
+};
