b89c833a23f3e7a25a47d0ecba8034265c747480
Le Kiet <kiethohohoho@gmail.com>
2023-07-01 23:46:00 +0700

    permission nav

--- src/layouts/dashboard/config-navigation.js
+++ src/layouts/dashboard/config-navigation.js
@@ -2,52 +2,53 @@ import { useMemo } from 'react';
 // routes
 import { paths } from 'src/routes/paths';
 // locales
-import { useLocales } from 'src/locales';
 // components
 // import Label from 'src/components/label';
-import SvgColor from 'src/components/svg-color';
+import { useAuthContext } from 'src/auth/hooks';
 
 // ----------------------------------------------------------------------
 
-const icon = (name) => (
-  <SvgColor src={`/assets/icons/navbar/${name}.svg`} sx={{ width: 1, height: 1 }} />
-  // OR
-  // <Iconify icon="fluent:mail-24-filled" />
-  // https://icon-sets.iconify.design/solar/
-  // https://www.streamlinehq.com/icons
-);
+// const icon = (name) => (
+//   <SvgColor src={`/assets/icons/navbar/${name}.svg`} sx={{ width: 1, height: 1 }} />
+//   // OR
+//   // <Iconify icon="fluent:mail-24-filled" />
+//   // https://icon-sets.iconify.design/solar/
+//   // https://www.streamlinehq.com/icons
+// );
 
-const ICONS = {
-  job: icon('ic_job'),
-  blog: icon('ic_blog'),
-  chat: icon('ic_chat'),
-  mail: icon('ic_mail'),
-  user: icon('ic_user'),
-  file: icon('ic_file'),
-  lock: icon('ic_lock'),
-  tour: icon('ic_tour'),
-  order: icon('ic_order'),
-  label: icon('ic_label'),
-  blank: icon('ic_blank'),
-  kanban: icon('ic_kanban'),
-  folder: icon('ic_folder'),
-  banking: icon('ic_banking'),
-  booking: icon('ic_booking'),
-  invoice: icon('ic_invoice'),
-  product: icon('ic_product'),
-  calendar: icon('ic_calendar'),
-  disabled: icon('ic_disabled'),
-  external: icon('ic_external'),
-  menuItem: icon('ic_menu_item'),
-  ecommerce: icon('ic_ecommerce'),
-  analytics: icon('ic_analytics'),
-  dashboard: icon('ic_dashboard'),
-};
+// const ICONS = {
+//   job: icon('ic_job'),
+//   blog: icon('ic_blog'),
+//   chat: icon('ic_chat'),
+//   mail: icon('ic_mail'),
+//   user: icon('ic_user'),
+//   file: icon('ic_file'),
+//   lock: icon('ic_lock'),
+//   tour: icon('ic_tour'),
+//   order: icon('ic_order'),
+//   label: icon('ic_label'),
+//   blank: icon('ic_blank'),
+//   kanban: icon('ic_kanban'),
+//   folder: icon('ic_folder'),
+//   banking: icon('ic_banking'),
+//   booking: icon('ic_booking'),
+//   invoice: icon('ic_invoice'),
+//   product: icon('ic_product'),
+//   calendar: icon('ic_calendar'),
+//   disabled: icon('ic_disabled'),
+//   external: icon('ic_external'),
+//   menuItem: icon('ic_menu_item'),
+//   ecommerce: icon('ic_ecommerce'),
+//   analytics: icon('ic_analytics'),
+//   dashboard: icon('ic_dashboard'),
+// };
 
 // ----------------------------------------------------------------------
 
 export function useNavData() {
-  const { t } = useLocales();
+  // const { t } = useLocales();
+
+  const { user: { role } } = useAuthContext();
 
   const data = useMemo(
     () => [
@@ -68,35 +69,40 @@ export function useNavData() {
       // MANAGEMENT
       // ----------------------------------------------------------------------
       {
-        subheader: t('management'),
+        // subheader: t('management'),
         items: [
+          (role === "Admin" && ({ title: "Danh sách tài khoản", path: paths.dashboard.user.list, })),
+          (role === "Admin" && ({ title: "Danh sách voucher", path: paths.dashboard.product.root, })),
+          (role === "Admin" && ({ title: "Tạo tài khoản", path: paths.dashboard.user.new, })),
+          (role === "Nhân viên" && ({ title: "Tạo voucher", path: paths.dashboard.product.new, })),
+
           // USER
-          {
-            title: t('user'),
-            path: paths.dashboard.user.root,
-            icon: ICONS.user,
-            children: [
-              // { title: t('profile'), path: paths.dashboard.user.root },
-              // { title: t('cards'), path: paths.dashboard.user.cards },
-              { title: t('list'), path: paths.dashboard.user.list },
-              { title: t('create'), path: paths.dashboard.user.new },
-              // { title: t('edit'), path: paths.dashboard.user.demo.edit },
-              // { title: t('account'), path: paths.dashboard.user.account },
-            ],
-          },
+          // {
+          //   title: t('user'),
+          //   path: paths.dashboard.user.root,
+          //   icon: ICONS.user,
+          //   children: [
+          //     { title: t('profile'), path: paths.dashboard.user.root },
+          //     { title: t('cards'), path: paths.dashboard.user.cards },
+          //     { title: t('list'), path: paths.dashboard.user.list },
+          //     { title: t('create'), path: paths.dashboard.user.new },
+          //     { title: t('edit'), path: paths.dashboard.user.demo.edit },
+          //     { title: t('account'), path: paths.dashboard.user.account },
+          //   ],
+          // },
 
           // PRODUCT
-          {
-            title: t('voucher'),
-            path: paths.dashboard.product.root,
-            icon: ICONS.product,
-            children: [
-              { title: t('list'), path: paths.dashboard.product.root },
-              // { title: t('details'), path: paths.dashboard.product.demo.details },
-              { title: t('create'), path: paths.dashboard.product.new },
-              // { title: t('edit'), path: paths.dashboard.product.demo.edit },
-            ],
-          },
+          // {
+          //   title: t('voucher'),
+          //   path: paths.dashboard.product.root,
+          //   icon: ICONS.product,
+          //   children: [
+          //     ({ title: t('list'), path: paths.dashboard.product.root }),
+          //     { title: t('details'), path: paths.dashboard.product.demo.details },
+          //     ({ title: t('create'), path: paths.dashboard.product.new }),
+          //     { title: t('edit'), path: paths.dashboard.product.demo.edit },
+          //   ]
+          // },
 
           // ORDER
           // {
@@ -196,10 +202,10 @@ export function useNavData() {
           //   path: paths.dashboard.kanban,
           //   icon: ICONS.kanban,
           // },
-        ],
+        ].filter(a => !!a),
       },
     ],
-    [t]
+    [role]
   );
 
   return data;
