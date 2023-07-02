d760792e08920f14122fd9f14d8830d553ecac01
Le Kiet <kiethohohoho@gmail.com>
2023-07-02 23:12:32 +0700

    vvvvvvvvvvvvvvvvvvvvv

--- src/sections/invoice/invoice-new-edit-form.js
+++ src/sections/invoice/invoice-new-edit-form.js
@@ -5,71 +5,50 @@ import { useForm } from 'react-hook-form';
 import * as Yup from 'yup';
 // @mui
 import LoadingButton from '@mui/lab/LoadingButton';
+import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import Stack from '@mui/material/Stack';
+import Grid from '@mui/material/Unstable_Grid2';
+// utils
 // routes
-import { useRouter } from 'src/routes/hook';
-import { paths } from 'src/routes/paths';
-// _mock
-import { _addressBooks } from 'src/_mock';
-// hooks
-import { useBoolean } from 'src/hooks/use-boolean';
+// assets
 // components
-import FormProvider from 'src/components/hook-form';
-//
+import FormProvider, { RHFAutocomplete, RHFCheckbox, RHFTextField } from 'src/components/hook-form';
+import { useSnackbar } from 'src/components/snackbar';
 import { useResponsive } from 'src/hooks/use-responsive';
 import { RouterLink } from 'src/routes/components';
-import InvoiceNewEditAddress from './invoice-new-edit-address';
-import InvoiceNewEditDetails from './invoice-new-edit-details';
-import InvoiceNewEditStatusDate from './invoice-new-edit-status-date';
+import axiosInstance, { endpoints } from 'src/utils/axios';
 
 // ----------------------------------------------------------------------
 
-export default function InvoiceNewEditForm({ currentInvoice }) {
+export default function UserNewEditForm({ currentInvoice }) {
+  const { enqueueSnackbar } = useSnackbar();
   const lgUp = useResponsive('up', 'lg');
 
-  const router = useRouter();
-
-  // const loadingSave = useBoolean();
-
-  const loadingSend = useBoolean();
-
   const NewInvoiceSchema = Yup.object().shape({
-    invoiceTo: Yup.mixed().nullable().required('Invoice to is required'),
-    createDate: Yup.mixed().nullable().required('Create date is required'),
-    dueDate: Yup.mixed()
-      .required('Due date is required')
-      .test(
-        'date-min',
-        'Due date must be later than create date',
-        (value, { parent }) => value.getTime() > parent.createDate.getTime()
-      ),
-    // not required
-    taxes: Yup.number(),
-    status: Yup.string(),
-    discount: Yup.number(),
-    shipping: Yup.number(),
-    invoiceFrom: Yup.mixed(),
-    totalAmount: Yup.number(),
-    invoiceNumber: Yup.string(),
+    voucher: Yup.string(),
+    customername: Yup.string().required('Bắt buộc điền Tên khách hàng'),
+    typeservice: Yup.string().required('Bắt buộc điền Loại dịch vụ'),
+    price: Yup.number().required('Bắt buộc điền Giá tiền'),
+    phone: Yup.string(),
+    customergroup: Yup.number(),
+    customerodd: Yup.boolean(),
+    notedvoucher: Yup.string(),
+    notedcustomer: Yup.string(),
   });
 
   const defaultValues = useMemo(
     () => ({
-      invoiceNumber: currentInvoice?.invoiceNumber || 'INV-1990',
-      createDate: currentInvoice?.createDate || new Date(),
-      dueDate: currentInvoice?.dueDate || null,
-      taxes: currentInvoice?.taxes || 0,
-      shipping: currentInvoice?.shipping || 0,
-      status: currentInvoice?.status || 'draft',
-      discount: currentInvoice?.discount || 0,
-      invoiceFrom: currentInvoice?.invoiceFrom || _addressBooks[0],
-      invoiceTo: currentInvoice?.invoiceTo || null,
-      items: currentInvoice?.items || [
-        { title: '', description: '', service: '', quantity: 1, price: 0, total: 0 },
-      ],
-      totalAmount: currentInvoice?.totalAmount || 0,
+      voucher: currentInvoice?.voucher || '',
+      customername: currentInvoice?.customername || '',
+      typeservice: currentInvoice?.typeservice || '',
+      price: currentInvoice?.price || '',
+      phone: currentInvoice?.phone || '',
+      customergroup: currentInvoice?.customergroup || '',
+      customerodd: currentInvoice?.customerodd || '',
+      notedvoucher: currentInvoice?.notedvoucher || '',
+      notedcustomer: currentInvoice?.notedcustomer || '',
     }),
     [currentInvoice]
   );
@@ -80,86 +59,95 @@ export default function InvoiceNewEditForm({ currentInvoice }) {
   });
 
   const {
-    reset,
-
     handleSubmit,
     formState: { isSubmitting },
   } = methods;
 
-  // const handleSaveAsDraft = handleSubmit(async (data) => {
-  //   loadingSave.onTrue();
-
-  //   try {
-  //     await new Promise((resolve) => setTimeout(resolve, 500));
-  //     reset();
-  //     loadingSave.onFalse();
-  //     router.push(paths.dashboard.invoice.root);
-  //     console.info('DATA', JSON.stringify(data, null, 2));
-  //   } catch (error) {
-  //     console.error(error);
-  //     loadingSave.onFalse();
-  //   }
-  // });
-
-  const handleCreateAndSend = handleSubmit(async (data) => {
-    loadingSend.onTrue();
-
+  const onSubmit = handleSubmit(async (data) => {
     try {
-      await new Promise((resolve) => setTimeout(resolve, 500));
-      reset();
-      loadingSend.onFalse();
-      router.push(paths.dashboard.invoice.root);
-      console.info('DATA', JSON.stringify(data, null, 2));
+
+      data.notedvoucher = data.notedvoucher.slice(0, -1) * 1;
+      if (currentInvoice) {
+        await axiosInstance.patch(endpoints.user.update, data);
+      } else {
+        await axiosInstance.post(endpoints.invoice.create, data);
+      }
+      enqueueSnackbar(currentInvoice ? 'Cập nhật thành công!' : 'Tạo thành công!');
     } catch (error) {
       console.error(error);
-      loadingSend.onFalse();
     }
   });
 
   return (
-    <FormProvider methods={methods}>
-      <Card>
-        <InvoiceNewEditAddress />
+    <FormProvider methods={methods} onSubmit={onSubmit}>
+      <Grid container spacing={3}>
+        <Grid xs={12} md={12}>
+          <Card sx={{ p: 3 }}>
+            <Box
+              rowGap={3}
+              columnGap={2}
+              display="grid"
+              gridTemplateColumns={{
+                xs: 'repeat(1, 1fr)',
+                sm: 'repeat(2, 1fr)',
+              }}
+            >
+              <RHFTextField name="voucher" label="Code giảm giá" />
+              <RHFTextField name="customername" label="Tên khách hàng" />
 
-        <InvoiceNewEditStatusDate />
+              <RHFAutocomplete
+                name="typeservice"
+                label="Loại dịch vụ"
+                options={['Vip combo', 'v5', 'v7', 'v9', 'v14', 'v18', 'v25']}
+                getOptionLabel={(option) => option}
+              />
 
-        <InvoiceNewEditDetails />
-      </Card>
+              <RHFTextField name="price" type="number" label="Giá tiền" />
 
-      <Stack justifyContent="flex-end" direction="row" spacing={2} sx={{ mt: 3 }}>
-        {/* <LoadingButton
-          color="inherit"
-          size="large"
-          variant="outlined"
-          loading={loadingSave.value && isSubmitting}
-          onClick={handleSaveAsDraft}
-        >
-          Save as Draft
-        </LoadingButton> */}
+              <RHFTextField name="phone" label="Số điện thoại" />
 
-        {!lgUp && <Button
-          size="large"
-          component={RouterLink}
-          href="/"
-          variant="contained"
-          sx={{ mt: "auto" }}>
-          Trở về trang chủ
-        </Button>}
+              <RHFTextField name="customergroup" label="Khách nhóm" />
 
-        <LoadingButton
-          size="large"
-          variant="contained"
-          loading={loadingSend.value && isSubmitting}
-          onClick={handleCreateAndSend}
-          sx={{ backgroundColor: 'rgb(0, 167, 111)' }}
-        >
-          {currentInvoice ? 'Cập nhật' : 'Tạo'}
-        </LoadingButton>
-      </Stack>
+              <RHFCheckbox name="customerodd" label="Khách lẻ" />
+
+              <RHFAutocomplete
+                name="notedvoucher"
+                label="Ghi chú khuyến mãi"
+                options={['20%', '30%', '50%', '100%']}
+              />
+
+              <RHFTextField name="notedcustomer" label="Ghi chú khách hàng" />
+            </Box>
+
+            <Stack
+              alignItems="flex-end"
+              direction="row"
+              justifyContent="flex-end"
+              gap={2}
+              sx={{ mt: 3 }}
+            >
+              {!lgUp && (
+                <Button component={RouterLink} href="/" variant="contained" sx={{ mt: 'auto' }}>
+                  Trở về trang chủ
+                </Button>
+              )}
+
+              <LoadingButton
+                type="submit"
+                variant="contained"
+                loading={isSubmitting}
+                sx={{ backgroundColor: 'rgb(0, 167, 111)' }}
+              >
+                {!currentInvoice ? 'Tạo' : 'Cập nhật'}
+              </LoadingButton>
+            </Stack>
+          </Card>
+        </Grid>
+      </Grid>
     </FormProvider>
   );
 }
 
-InvoiceNewEditForm.propTypes = {
+UserNewEditForm.propTypes = {
   currentInvoice: PropTypes.object,
 };
